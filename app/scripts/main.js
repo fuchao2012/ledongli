e3a0a27cd872a86a33c5c12f9a0c0a8f4e439117
fuchao2012 <zhengfuchao2008@126.com>
2015-09-14 00:12:53 +0800

    init page 1. zepto 2. slide 3. pictures 4. main.scss 5. slide-main-js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+slide({
+  slide_page_wrap: '.slide_div',
+  slide_page_dom: '.slide_page',
+  startCallback: function(scope) {
+    console.log('touch start', scope);
+  },
+  moveCallback: function(scope) {
+    console.log('touch move', scope);
+  },
+  endCallback: function(scope) {
+    console.log('touch end', scope);
+  }
+});
