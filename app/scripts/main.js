71c67f53db17ff3024a8d5a2f46d2b37f384359f
fuchao2012 <zhengfuchao2008@126.com>
2015-09-14 01:56:50 +0800

    add all pages 2 .list 3. detail 4. joined 5. myproject 6.share

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,4 @@
 slide({
   slide_page_wrap: '.slide_div',
-  slide_page_dom: '.slide_page',
-  startCallback: function(scope) {
-    console.log('touch start', scope);
-  },
-  moveCallback: function(scope) {
-    console.log('touch move', scope);
-  },
-  endCallback: function(scope) {
-    console.log('touch end', scope);
-  }
+  slide_page_dom: '.slide_page'
 });
